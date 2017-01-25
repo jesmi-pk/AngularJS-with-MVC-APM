a614653473455fd68062414567e67bda3f2afe12
jesmi.pk <jesmi.pk@CS-044-DT-WS>
2017-01-25 09:43:43 +0530

    [APM:11.0] Added initial layout,listing,top navigation

---
+++ APM.client/app/app.js
@@ -0,0 +1,19 @@
+﻿//(function(){
+//    "use strict";
+    var app=angular.module("productManagement",
+    ["ngRoute"]);
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "app/Home/home.html"
+              
+            })
+            .when("/product", {
+                templateUrl:"app/product/productListView.html",
+    controller: "app/product/productListCtrl"
+            })
+            .otherwise({
+                redirectTo: "/"
+            });
+    
+});
---
+++ APM.client/app/layout/sidebar.js
@@ -0,0 +1,19 @@
+﻿(function(app) {
+    'use strict';
+    angular
+       .module("productManagement")
+       .controller("sidebar",
+    sidebar);
+ 
+    //app.directive('sideBar', sideBar);
+ 
+    function sidebar() {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: '/app/layout/sidebar.html'
+
+        }
+    }
+    
+    })
